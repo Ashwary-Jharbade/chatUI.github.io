263912f5c2e933d7d13c099c3e1d42ae55526cb4
Ashwary <ashwary.jharbade999@gmail.com>
2021-08-19 17:48:49 +0530

    added user side texting functionality

--- js/script.js
+++ js/script.js
@@ -1,12 +1,102 @@
-const fullSize = (event) => {
-    event.target.requestFullscreen();
-}
+const getEle = (ele, cls = "") => {
+  let obj = document.createElement(ele);
+  obj.classList = cls;
+  return obj;
+};
 
-window.onresize = () => {
-    let hobj = document.querySelector('.people-list');
-    hobj.style.height = (screen.height - 150) + "px"
-    console.log(screen.height - 150);
-}
+const createChat = (msg, time, side) => {
+  let ele = "div";
+  let obj1 = getEle(ele, side);
+  let obj2 = getEle(ele);
+  let obj3 = getEle(ele, "time-left");
+  obj2.innerHTML = msg;
+  obj3.innerHTML = time;
+  obj1.append(obj2);
+  obj1.append(obj3);
+  return obj1;
+};
+
+const addPrevChats = (chats, obj) => {
+  let left = "message-left";
+  let right = "message-right";
+  chats.forEach((item) => {
+    let temp;
+    if (item.client === "user") {
+      temp = createChat(item.message, item.time, right);
+    } else {
+      temp = createChat(item.message, item.time, left);
+    }
+    obj.append(temp);
+  });
+};
+
+const fullSize = (event) => {
+  event.target.requestFullscreen();
+};
+
+const renderChat = () => {
+  let width = document.body.clientWidth;
+  if (width <= 601) {
+    let obj = document.getElementById("chat_section");
+    obj.style.display = "block";
+    obj.style.width = "100%";
+    let sobj = document.getElementById("chat_list");
+    sobj.style.display = "none";
+  }
+};
+
+const showList = () => {
+  let width = document.body.clientWidth;
+  if (width <= 601) {
+    let obj = document.getElementById("chat_section");
+    obj.style.display = "none";
+    let sobj = document.getElementById("chat_list");
+    sobj.style.display = "block";
+  }
+};
+
+const getNormalizeTime = () => {
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    if (hr > 12) {
+        return `${ hr-12 }:${ min }pm`
+    }
+    return `${hr}:${min}pm`
+} 
+
+
+// const onEntered = (event) => {
+//     if(event.key === "Enter") {
+//         addUserMessage()
+//     }
+// }
+
+const addUserMessage = () => {
+    let msg = document.getElementById("user_text");
+    if ( msg.value ) {
+        let time = getNormalizeTime();
+        let cobj = document.querySelector(".chat-surface");
+        let obj = createChat(msg.value,time,"message-right");
+        cobj.append(obj);
+        obj.scrollIntoView();
+    } else {
+        alert("Message is not valid!");
+    }
+    msg.value = "";
+};
+
+window.addEventListener("resize", () => {
+  let width = document.body.clientWidth;
+  if (width > 601) {
+    let obj = document.getElementById("chat_section");
+    obj.style.display = "block";
+    let sobj = document.getElementById("chat_list");
+    sobj.style.display = "block";
+  } else {
+    showList();
+  }
+});
 
 const listItems = `
 <li>
@@ -28,16 +118,19 @@ const listItems = `
             </div>
         </div>
     </div>
-</li>`
+</li>`;
 
-window.onload = () =>{
-    let obj = document.querySelectorAll('.people-list-fill');
-    obj.forEach((item)=>{
-        item.innerHTML = listItems;
-    })
-
-    let hobj = document.querySelector('.people-list');
-    hobj.style.height = (screen.height - 150) + "px"
-    console.log(screen.height - 150);
-}
-
+window.onload = () => {
+  let obj = document.querySelectorAll(".people-list-fill");
+  obj.forEach((item) => {
+    item.innerHTML = listItems;
+  });
+  let cobj = document.querySelector(".chat-surface");
+  fetch("../json/chats.json")
+    .then((response) => response.json())
+    .then((data) => {
+      chats = data.data;
+      addPrevChats(chats, cobj);
+    });
+  cobj.scrollTop = cobj.scrollHeight;
+};
